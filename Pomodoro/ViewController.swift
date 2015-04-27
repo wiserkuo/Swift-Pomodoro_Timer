f65fb00510376ed5c52a1eb00f32aa88949e62a6
wiserkuo <wiserkuo@wiserkuos-MacBook-Pro.local>
2015-04-27 20:58:42 +0800

    NSTimer ,NSTimeInterval
    Basic 25 mins countdown label

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    var startTime = NSTimeInterval()
+    var targetTime = 25*60
+    var timer = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        startTime = NSDate.timeIntervalSinceReferenceDate()
     }
+    func updateTime(){
+        let endTime = NSDate.timeIntervalSinceReferenceDate()
+        let time = Int(endTime-startTime)
+        if time >= targetTime {
+            timer.invalidate()
+        }
+        let min = (targetTime-time)/60 > 9 ? String((targetTime-time)/60) : "0"+String((targetTime-time)/60)
+        let sec = (targetTime-time)%60 > 9 ? String((targetTime-time)%60) : "0"+String((targetTime-time)%60)
 
+        timeLabel.text = "\(min):\(sec)"
+        println("time=\(time) , count down=\(min):\(sec)")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
