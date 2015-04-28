e3882cf71ca4720ea1dea291126903358a2e3ee7
wiserkuo <wiserkuo@wiserkuos-MacBook-Pro.local>
2015-04-28 22:39:03 +0800

    Completed function with 25:00 work and 5:00 rest
    It implement a completed function to loop between 25 and 15 mins
    work/rest countdown.
    Start button is used to start countdown , repeat tap will let it
    restart.
    End button is used to end countdown, then switch to another state
    (work/rest).
    Usage of UIColor(red,green,blue,alpha)

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -11,26 +11,70 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     var startTime = NSTimeInterval()
-    var targetTime = 25*60
+    var targetTime :Int?
     var timer = NSTimer()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+    var status :Int?
+    @IBAction func startTapped(sender: AnyObject) {
+        println("start")
+        timer.invalidate()
+        if status == 0 {
+            timeLabel.text = "25:00"
+            targetTime = 25*60
+        }
+        else if status == 1 {
+            timeLabel.text = "05:00"
+            targetTime = 5*60
+        }
+        println("\(timeLabel.text)")
+        timer.invalidate()
         startTime = NSDate.timeIntervalSinceReferenceDate()
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func endTapped(sender: AnyObject) {
+        println("end")
+        if status == 0 {
+            status = 1
+            timeLabel.text = "05:00"
+            self.view.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
+        }
+        else {
+            status = 0
+            timeLabel.text = "25:00"
+            self.view.backgroundColor = UIColor(red: 0.9, green: 0, blue: 0, alpha: 1)
+        }
+        timer.invalidate()
     }
     func updateTime(){
         let endTime = NSDate.timeIntervalSinceReferenceDate()
         let time = Int(endTime-startTime)
         if time >= targetTime {
+            if status == 0 {
+                status = 1
+                timeLabel.text = "05:00"
+                self.view.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
+            }
+            else {
+                status = 0
+                timeLabel.text = "25:00"
+                self.view.backgroundColor = UIColor(red: 0.7, green: 0, blue: 0, alpha: 1)
+            }
             timer.invalidate()
         }
-        let min = (targetTime-time)/60 > 9 ? String((targetTime-time)/60) : "0"+String((targetTime-time)/60)
-        let sec = (targetTime-time)%60 > 9 ? String((targetTime-time)%60) : "0"+String((targetTime-time)%60)
-
+        let min = (targetTime!-time)/60 > 9 ? String((targetTime!-time)/60) : "0"+String((targetTime!-time)/60)
+        let sec = (targetTime!-time)%60 > 9 ? String((targetTime!-time)%60) : "0"+String((targetTime!-time)%60)
+        
         timeLabel.text = "\(min):\(sec)"
         println("time=\(time) , count down=\(min):\(sec)")
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 0.9, green: 0, blue: 0, alpha: 1)
+        status=0
+        targetTime = 25*60
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
     override func didReceiveMemoryWarning() {
